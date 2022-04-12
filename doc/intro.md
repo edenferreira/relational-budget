6cb90204614f5dc43618f3f2a2ad656bced7ddf5
Éden Ferreira <thiago_eden@hotmail.com>
2022-04-12 13:56:48 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to orcamento
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/edenferreira/orcamento_test.clj
@@ -0,0 +1,7 @@
+(ns edenferreira.orcamento-test
+  (:require [clojure.test :refer :all]
+            [edenferreira.orcamento :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
