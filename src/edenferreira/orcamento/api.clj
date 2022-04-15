0b11e36af13788e177ffa4eb8665bf5419137e32
Éden Ferreira <thiago_eden@hotmail.com>
2022-04-15 08:35:55 -0300

    implementing create category

--- src/edenferreira/orcamento/api.clj
+++ src/edenferreira/orcamento/api.clj
@@ -1,6 +1,7 @@
 (ns edenferreira.orcamento.api
   (:require [br.com.orcamento :as-alias orc]
             [br.com.orcamento.budget :as-alias budget]
+            [br.com.orcamento.category :as-alias category]
             [br.com.orcamento.entry :as-alias entry]))
 
 (defn create-budget [budget-name as-of db]
@@ -8,3 +9,13 @@
           (fnil conj #{})
           #::budget{:name budget-name
                     :created-at as-of}))
+
+(defn create-category [category-name as-of db]
+  (update db ::orc/categories
+          (fnil conj #{})
+          #::category{:name category-name
+                      :created-at as-of}))
+
+(defn create-account [])
+
+(defn add-entry [])
--- test/edenferreira/orcamento/api_test.clj
+++ test/edenferreira/orcamento/api_test.clj
@@ -2,17 +2,31 @@
   (:require [clojure.test :refer [deftest testing is]]
             [br.com.orcamento :as-alias orc]
             [br.com.orcamento.budget :as-alias budget]
-            [edenferreira.orcamento.api :as api]))
+            [br.com.orcamento.category :as-alias category]
+            [edenferreira.orcamento.api :as api]
+            [matcher-combinators.test :refer [match?]]))
 
 (deftest create-budget
-  (is (= {::orc/budgets
-           #{#::budget{:name "some budget"
-                       :created-at #inst "2000-01-01T00:00:00Z"}}}
-         (api/create-budget
-          "some budget"
-          #inst "2000-01-01T00:00:00Z"
-          {}))))
+  (is (match?
+       {::orc/budgets
+        #{#::budget{:name "some budget"
+                    :created-at #inst "2000-01-01T00:00:00Z"}}}
+       (api/create-budget
+        "some budget"
+        #inst "2000-01-01T00:00:00Z"
+        {}))))
+
+(deftest create-category
+  (is (match?
+       {::orc/categories
+        #{#::category{:name "category name"
+                      :created-at #inst "2000-01-01T00:00:00Z"}}}
+       (api/create-category
+        "category name"
+        #inst "2000-01-01T00:00:00Z"
+        {}))))
 
 
 (comment
+  (edev/e-la-vamos-nos)
   )
