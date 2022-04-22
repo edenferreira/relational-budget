dd9c76227ba5fc6123e12a4cfcc0e3b9e6d7402c
Éden Ferreira <thiago_eden@hotmail.com>
2022-04-22 18:36:54 -0300

    setting up as-of filter

--- src/edenferreira/rawd/api.clj
+++ src/edenferreira/rawd/api.clj
@@ -3,6 +3,7 @@
   (:require [clojure.string :as str]
             [hiccup.page :as h.page]
             [hiccup.core :as h]
+            [edenferreira.rawd.instant :as instant]
             [clojure.set :as set]))
 
 ;; slightly modified from https://stackoverflow.com/questions/12679754/idiomatic-way-of-rendering-style-info-using-clojure-hiccup
@@ -38,6 +39,17 @@
              :style (style :margin "8px")}
     (map #(do [:option {:value %} %])
          options)]])
+
+(defn create-datetime-input [& {:keys [id form label name value placeholder]}]
+  [:div {:class "form-group"}
+   [:label {:for id
+            :style (style :margin-right "8px")} label]
+   [:input (cond-> {:type "datetime-local"
+                    :id id
+                    :name name
+                    :form form}
+             value (assoc :value value)
+             placeholder (assoc :placeholder placeholder))]])
 
 (defn create-input [& {:keys [id form type label name placeholder]}]
   [:div {:class "form-group"}
@@ -106,7 +118,7 @@
   [:div {:class "row"} (map entity->form entities)])
 
 (defn create-index [get-state entities]
-  (fn index [_request]
+  (fn index [request]
     (h.page/html5
      {}
      (h/html
@@ -120,13 +132,34 @@
         [:h1 {:class ""} "Rawd"]
         (entities->forms entities)
         [:hr]
+        [:h1 {:class "text-success"} "Filters"]
+        (let [form-id "as-of-filter-form"]
+          (create-form
+           :id form-id
+           :enctype "application/x-www-form-urlencoded"
+           :method "POST"
+           :action "/filter/as-of"
+           :items [(create-datetime-input :id "as-of-filter"
+                                          :form form-id
+                                          :label "as-of"
+                                          :name "as-of"
+                                          :value (when (get-in request [:params :as-of])
+                                                   (subs (str (get-in request [:params :as-of]))
+                                                         0
+                                                         (- (count (str (get-in request [:params :as-of])))
+                                                            4))))]
+           :button-label "Filter"))
+
+        [:hr]
         [:h1 {:class "text-success"} "State"]
         (db->table (get-state))]]))))
 
 (defn create-respond-index [get-state entities]
   (let [index (create-index get-state entities)]
     (fn respond-index [request]
-      {:status 200 :body (index request)})))
+      {:status 200 :body (index (update-in request
+                                           [:params :as-of]
+                                           instant/parse))})))
 
 (defn respond-create-entity [entity adapter handler]
   (fn create-entity [request]
@@ -138,7 +171,7 @@
           handled (handler adapted)]
       (if (= 200 (:status handled))
         {:status 303
-         :headers {"Location" "/index.html"}
+         :headers {"Location" "/"}
          :body {}}
         handled))))
 
@@ -156,12 +189,21 @@
 
 (defn routes [interceptors get-state entities]
   (set/union
-   #{["/index.html" :get (conj interceptors (create-respond-index get-state entities)) :route-name :index]}
+   #{["/" :get (conj interceptors (create-respond-index get-state entities)) :route-name :index]
+     ["/index.html" :get (conj interceptors (create-respond-index get-state entities)) :route-name :index-html]
+     ["/filter/as-of" :post (conj interceptors
+                                  (fn [request]
+                                    {:status 303
+                                     :headers {"Location" (str "/?as-of=" (str (get (:params request) "as-of")
+                                                                               ":00Z"))}
+                                     :body {}}))
+      :route-name :filter-as-of]}
    (entities->routes interceptors entities)))
 
 (comment
   (->> (db->table {:c #{{:a 1 :b 2}
                         {:a 2 :b 3}}})
-       (h.page/html5 {}))
+       (h.html5/page {}))
   (do h)
+  (do ppp)
   '_)
